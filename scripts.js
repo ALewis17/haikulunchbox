b1ea1125230e9ad4cbbc49a9a44dfde812089c47
ALewis17 <contact@austinlewis.design>
2020-12-06 01:21:16 -0800

    Ver. 1.1 Mobile updates, script updates

--- scripts.js
+++ scripts.js
@@ -1,25 +1,39 @@
 var haiku = [
-    'smells of food waft by<br>and I am far and away<br>be back in thirty',
+    "scents of food waft by<br>and I am far and away<br>be back in thirty",
     "I see you ridin'<br>round town with the food I want<br>and I'm like, pho you",
-    'Pears'
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten"
 ]
 
 
+//haiku function
 function newHaiku() {
     var randomNumber = Math.floor(Math.random() * (haiku.length));
     document.getElementById('haikuDisplay').innerHTML = haiku[randomNumber];
 }
 
+//copy function
 function CopyToClipboard(containerid) {
     if (document.selection) {
-      var range = document.body.createTextRange();
-      range.moveToElementText(document.getElementById(containerid));
-      range.select().createTextRange();
-      document.execCommand("copy");
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy");
     } else if (window.getSelection) {
-      var range = document.createRange();
-      range.selectNode(document.getElementById(containerid));
-      window.getSelection().addRange(range);
-      document.execCommand("copy");
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
     }
-  }
+}
+
+
+
+
