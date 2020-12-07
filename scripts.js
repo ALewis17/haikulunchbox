0d2e264eb0302e7e1d19916a8cd6ea7817745ede
ALewis17 <contact@austinlewis.design>
2020-12-06 18:38:47 -0800

    10 finsihed haikus and trying to get mobile to display just right

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 var haiku = [
     "scents of food waft by<br>and I am far and away<br>be back in thirty",
     "I see you ridin'<br>round town with the food I want<br>and I'm like, pho you",
-    "Three",
-    "Four",
-    "Five",
-    "Six",
-    "Seven",
-    "Eight",
-    "Nine",
-    "Ten"
+    "thoughts of wagyu beef<br>how much does it cost again?<br>a salad it is",
+    "soup or a salad<br>cannot decide which to get<br>both is the answer",
+    "no milk once again<br>it's more likely than you think<br>at the Dairy Queen",
+    "coffee is not a food<br>but got dayum is it good<br>second pot today",
+    "Door Dash on the way<br>they have two more stops to make<br>icecream is melted",
+    "trail mix is a treat<br>is that a raisin I see<br>that is not a treat",
+    "another meeting<br>but the clock says it is noon<br>my sandwich awaits",
+    "the microwave beeps<br>my mouth starts to salivate<br>is this Pavlov's Dog"
 ]
 
 
